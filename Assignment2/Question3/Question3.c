760de13786cb9d827cd43901f59d7445cf4311c2
Mohmmad Burair <aliburair87@gmail.com>
2024-11-27 21:17:47 +0500

    Update Question3.c

--- Assignment2/Question3/Question3.c
+++ Assignment2/Question3/Question3.c
@@ -1,1 +1,44 @@
+/* 
+   Name: Mohammad Burair
+   Roll Number: 24k-0775
+   Section: BCS-1C
+   Description: Program to compress a word by removing consecutive duplicate characters.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_WORD_LENGTH 100
+
+int compressWord(char *word, char *compressedWord) { 
+    int removedCount = 0; 
+    int j = 0;  // Index for the compressed word
+
+    for (int i = 0; i < strlen(word); i++) { 
+        if (i == 0 || word[i] != word[i - 1]) { 
+            compressedWord[j++] = word[i]; 
+        } else { 
+            removedCount++; 
+        } 
+    }
+
+    compressedWord[j] = '\0'; 
+    return removedCount; 
+}
+
+int main() { 
+    
+    char word[MAX_WORD_LENGTH]; 
+    char compressedWord[MAX_WORD_LENGTH];
+
+    printf("Enter a word: "); 
+    scanf("%s", word);
+
+    int removedCount = compressWord(word, compressedWord);
+
+    printf("Compressed word: %s\n", compressedWord); 
+    printf("Total characters removed: %d\n", removedCount);
+
+    return 0; 
+}
 
